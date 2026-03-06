880b1909015e83bea2240fe006d0427a018d95f7
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-06 12:17:15 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,59 @@
-# terraform/outputs.tf
-output "resource_group_name" {
-  description = "Name of the created resource group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "arm_deployment_name" {
-  description = "Name of the ARM deployment"
+# -------------------------------------------------------
+# Storage Account Outputs
+# -------------------------------------------------------
+output "storage_deployment_name" {
   value       = azurerm_resource_group_template_deployment.storage.name
+  description = "Storage ARM deployment name"
 }
 
-output "arm_deployment_outputs" {
-  description = "Outputs from the ARM template"
+output "storage_deployment_outputs" {
   value       = jsondecode(azurerm_resource_group_template_deployment.storage.output_content)
+  description = "Storage account deployment outputs"
+}
+
+# -------------------------------------------------------
+# VMSS Outputs
+# -------------------------------------------------------
+output "vmss_deployment_name" {
+  value       = azurerm_resource_group_template_deployment.vmss.name
+  description = "VMSS ARM deployment name"
+}
+
+output "vmss_deployment_outputs" {
+  value       = jsondecode(azurerm_resource_group_template_deployment.vmss.output_content)
+  description = "VMSS deployment outputs including load balancer public IP"
+}
+
+# -------------------------------------------------------
+# ADF Outputs
+# -------------------------------------------------------
+output "adf_deployment_name" {
+  value       = azurerm_resource_group_template_deployment.adf.name
+  description = "ADF ARM deployment name"
+}
+
+output "adf_deployment_outputs" {
+  value       = jsondecode(azurerm_resource_group_template_deployment.adf.output_content)
+  description = "ADF deployment outputs including factory ID and principal ID"
+}
+
+# -------------------------------------------------------
+# ADF Pipeline & Trigger Outputs
+# -------------------------------------------------------
+output "adf_pipeline_deployment_name" {
+  value       = azurerm_resource_group_template_deployment.adf_pipeline.name
+  description = "ADF pipeline ARM deployment name"
+}
+
+output "adf_pipeline_outputs" {
+  value       = jsondecode(azurerm_resource_group_template_deployment.adf_pipeline.output_content)
+  description = "ADF pipeline and trigger deployment outputs"
+}
+
+# -------------------------------------------------------
+# Summary
+# -------------------------------------------------------
+output "resource_group_name" {
+  value       = data.azurerm_resource_group.main.name
+  description = "Resource group all resources are deployed to"
 }
