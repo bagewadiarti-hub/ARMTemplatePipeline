9392e93603b6ac4df9c99c61004442e3cc13dfe5
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-06 12:16:47 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,82 @@
-# terraform/variables.tf
-variable "resource_group_name" {
-  description = "Name of the resource group"
+# -------------------------------------------------------
+# General
+# -------------------------------------------------------
+variable "environment" {
   type        = string
+  description = "Target environment (dev / staging / prod)"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Existing resource group name"
   default     = "rg-demo-resources"
 }
 
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "westus2"
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "dev"
-}
-
+# -------------------------------------------------------
+# Storage Account
+# -------------------------------------------------------
 variable "storage_account_name" {
-  description = "Storage account name"
   type        = string
+  description = "Name of the storage account (used for ADF linked service too)"
+  default     = "stgdemostorage179"
+}
+
+variable "storage_account_key" {
+  type        = string
+  sensitive   = true
+  description = "Primary key of the storage account (used by ADF linked service)"
+}
+
+# -------------------------------------------------------
+# VMSS
+# -------------------------------------------------------
+variable "vmss_name" {
+  type        = string
+  description = "Name of the Virtual Machine Scale Set"
+  default     = "vmss-demo-arm-101r"
+}
+
+variable "vmss_admin_username" {
+  type        = string
+  description = "Admin username for VMSS instances"
+  default     = "azureuser"
+}
+
+variable "vmss_admin_password" {
+  type        = string
+  sensitive   = true
+  description = "Admin password for VMSS instances (min 12 chars, mixed case + number + special)"
+}
+
+variable "vmss_instance_count" {
+  type        = number
+  description = "Number of VMSS instances to deploy"
+  default     = 1
+}
+
+# -------------------------------------------------------
+# Azure Data Factory
+# -------------------------------------------------------
+variable "adf_name" {
+  type        = string
+  description = "Name of the Azure Data Factory (must be globally unique)"
+  default     = "adf-demo-pipeline-dev"
+}
+
+variable "adf_source_container" {
+  type        = string
+  description = "Source blob container name for ADF copy pipeline"
+  default     = "source-container"
+}
+
+variable "adf_destination_container" {
+  type        = string
+  description = "Destination blob container name for ADF copy pipeline"
+  default     = "destination-container"
+}
+
+variable "adf_trigger_start_time" {
+  type        = string
+  description = "Schedule trigger start time in UTC ISO 8601 format"
+  default     = "2026-01-01T00:00:00Z"
 }
